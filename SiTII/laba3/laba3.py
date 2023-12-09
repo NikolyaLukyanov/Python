d332210d9ebb4cea3568f344599fc88559361035
NikolyaLukyanov <nikolya.lukyanov.02@mail.ru>
2023-12-09 21:51:51 +0300

    Update laba3.py

--- SiTII/laba3/laba3.py
+++ SiTII/laba3/laba3.py
@@ -1,40 +1,22 @@
 import pandas as pd
-import csv
 
-# with open('944_corrupted_incidents.csv', encoding="UTF-8") as csv_file:
-#     table = csv.reader(csv_file)
-#     log_data = {
-#         'id': [],
-#         'name': [],
-#         'description': [],
-#         'type': [],
-#         'events_count': [],
-#         'crit_rate': [],
-#         'assets_id': [],
-#         'vulnerabilities_id': [],
-#         'start_time': [],
-#         'end_time': []
-#     }
-#
-#     for i, row in enumerate(table):
-#         if i == 0:
-#             continue
-#         log_data['id'].append(row[1])
-#         log_data['name'].append(row[2])
-#         log_data['description'].append(row[3])
-#         log_data['type'].append(row[4])
-#         log_data['events_count'].append(row[5])
-#         log_data['crit_rate'].append(row[6])
-#         log_data['assets_id'].append(row[7])
-#         log_data['vulnerabilities_id'].append(row[8])
-#         log_data['start_time'].append(row[9])
-#         log_data['end_time'].append(row[10])
-# df = pd.DataFrame(log_data)
 df = pd.read_csv('944_corrupted_incidents.csv')
 # Убираем дубликаты
 df.drop_duplicates(
-    subset=['name', 'description', 'type', 'events_count', 'crit_rate', 'assets_id', 'vulnerabilities_id', 'start_time', 'end_time'],
+    subset=['name', 'description', 'type', 'events_count', 'crit_rate', 'assets_id', 'vulnerabilities_id', 'start_time',
+            'end_time'],
     inplace=True,
     ignore_index=True,
 )
-print(df)
+# Исправляем description
+count = 0
+while count < len(df['type'].values):
+    df['description'].values[count] = f'Обнаружена активность "{df['type'].values[count]}" с уязвимостями {str(df['vulnerabilities_id'].values[count].replace('[','').replace(']','').replace("'","").replace("'",""))}'
+    count += 1
+
+
+df = df.loc[df['events_count'] > 0]
+df = df.dropna()
+
+
+df.to_csv('csv_copy.csv', index=False)
